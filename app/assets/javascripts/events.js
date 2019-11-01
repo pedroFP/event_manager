273f3823d79d13805647fe4987f420ae6ca53a54
pedroFP <pedro.araya.labra@gmail.com>
2019-10-31 23:22:31 -0300

    FIXED views

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,36 +1,3 @@
 $(document).ready(function() {
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').focus()
-  })
-
-  $('.edit').on('click', function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: '/events/' + this.id + '/edit',
-      type: 'GET',
-      dataType: 'script'
-    })
-    .done(function() {
-      console.log("success");
-    })
-    .fail(function() {
-      console.log("error");
-    })
-  });
-
-  $('.new').on('click', function(event) {
-    event.preventDefault();
-    $.ajax({
-      url: '/events/new',
-      type: 'GET',
-      dataType: 'script'
-    })
-    .done(function() {
-      console.log("success");
-    })
-    .fail(function() {
-      console.log("error");
-    })
-  });
   
 });
