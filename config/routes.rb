e4c3a43169585b2d16dc4809706e5b58e00098a6
pedroFP <pedro.araya.labra@gmail.com>
2019-11-01 04:45:29 -0300

    FIXED route and UPRGADE views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
   resources :events do
-    resources :tickets, only: :index
+    resources :tickets, only: [:index, :create]
   end
 
-  resources :tickets, only: [:index, :create,  :show]
+  resources :tickets, only: [:index, :show]
   root 'events#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
